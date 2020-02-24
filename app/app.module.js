48d5fbb872c4f58e630bcebef1456a0ddd2fb1f0
Arvin Lleva <arvin.lleva@yahoo.com>
2020-02-23 17:40:55 -0800

    Messing with custom directives

--- app/app.module.js
+++ app/app.module.js
@@ -9,10 +9,12 @@ myApp.config(['$routeProvider', function($routeProvider) {
     .when('/home', {
       // ! when using templateURL, it's important that you start from the root directory when calling it
       // ! because it is usually for external template file
-      templateUrl: './app/components/home/homeView.html'
+      templateUrl: './app/components/home/homeView.html',
+      controller: 'AppController'
     })
     .when('/list', {
       templateUrl: './app/components/list/listView.html',
+      // this controller is what is used to controler the listView
       controller: 'AppController'
     })
     .when('/experimental', {
@@ -25,18 +27,54 @@ myApp.config(['$routeProvider', function($routeProvider) {
 
 }]);
 
+// this is to create a custom directive, and we use camelCase in the js file while it'll be a snake case in the html
+myApp.directive('randomPerson',['$http', function($http) {  // this object that the directive is return is what will control everything
+  return {
+    restrict: 'E',
+    // this directive has it's own scope
+    scope: {
+      // the equal sign means that it'll bind it
+      people: '=',
+      title: '=',
+      length: '='
+    },
+
+    // we can use this to show the view
+  
+
+    templateUrl: 'app/components/random/random.html',
+
+    // this scope is refering to the scope of this directive
+    // we can also make the controller be 'AppController' or any other controller
+    controller: function($scope) {
+      $http.get('assets/data/people.json')
+      .then(function successCallback(response) {
+        var people = response.data;
+        var size = response.data.length;
+        var randomIndex = Math.floor(Math.random() * size);
+        $scope.randomPerson =  people[randomIndex];
+
+        console.log($scope.randomPerson, "what");
+        }, function errorCallback(response) {
+          console.log("Status:",response.statusText);
+        });
+    }
+    // you can make your controller like this
+  };
+}])
 
 // this function will trigger when it runs
 // myApp.run(function() {
 
 // });
+
 // this is will the dependencies and the function when you wrap the second parameter as an array
 // because sometime we might have multiple controllers with the same name of scopes
 // this sets the initial state
 myApp.controller('AppController',['$scope', '$http', function($scope, $http) {
   $scope.message ="Hi";
   $scope.ninjas = ['don', 'mike', 'leo'];
-
+  
   $scope.addPerson = function() {
     var newPerson = {
       name: $scope.newPerson.name,
@@ -64,16 +102,22 @@ myApp.controller('AppController',['$scope', '$http', function($scope, $http) {
     });
     $scope.people = newPeople;
   };
-
+  
   // this is how to get data from an api
+  // recall that this is an asynchonous function
   $http.get('assets/data/people.json')
-    .then(function successCallback(response) {
-      $scope.people = response.data
-      console.log("Status:", response.statusText)
+  .then(function successCallback(response) {
+    
+    $scope.people = response.data;
+    $scope.length = $scope.people.length;
+    $scope.randomPersonIndex = Math.floor(Math.random() * $scope.people.length);
+    $scope.randomPerson = $scope.people[$scope.randomPersonIndex];
+
+    console.log("Status:", response.statusText)
     }, function errorCallback(response) {
       console.log("Status:",response.statusText);
     });
-  
+
 
   // we wont be using this anymore because we'll be making a http request to get the data
   // $scope.people = [
